3e8e81b389fe722f395fb7a303b30a38986fb5cf
ledengx <cnxxgy@163.com>
2023-12-08 23:01:58 +0800

    first commit

---
+++ src/test/java/com/web3/degovern/DeGovernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web3.degovern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeGovernApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
